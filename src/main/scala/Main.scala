9b94a29db60250c09ba3dff3095864a4c7673461
Aaron J. Lang <aaronjameslang@googlemail.com>
2017-12-12 14:17:54 +1300

    Add basic OCR and tests

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,11 @@
+package com.aaronjameslang.cormorant
+
+import java.io.File
+import net.sourceforge.tess4j.Tesseract
+
 object Main extends App {
   println("Hello, World!")
+
+  def textFromImage(filepath: String): String =
+    new Tesseract().doOCR(new File(filepath))
 }
